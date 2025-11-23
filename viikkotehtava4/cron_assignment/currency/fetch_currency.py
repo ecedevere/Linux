54bfbc76563ba338ca0ba084ebd0234d0399bdb9
ecedevere <sheykina.mariia@gmail.com>
2025-11-23 13:00:49 +0000

    viikkotehtava4:salasana poistettu

--- viikkotehtava4/cron_assignment/currency/fetch_currency.py
+++ viikkotehtava4/cron_assignment/currency/fetch_currency.py
@@ -17,7 +17,7 @@ URL = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/EUR"
 conn = pymysql.connect(
     host="localhost",
     user="weather_user",
-    password="4Rg5!33tt",
+    password=os.getenv("DB_PASS"),
     database="weather_db",
     charset="utf8mb4"
 )
--- viikkotehtava4/cron_assignment/fetch_weather.py
+++ viikkotehtava4/cron_assignment/fetch_weather.py
@@ -17,7 +17,7 @@ URL = f"https://api.openweathermap.org/data/2.5/weather?q=Oulu&appid={API_KEY}&u
 conn = pymysql.connect(
     host="localhost",
     user="weather_user",
-    password="4Rg5!33tt",
+    password=os.getenv("DB_PASS"),
     database="weather_db",
     charset="utf8mb4"
 )
